3e2f86d8abdbd890736428b06d33a2ee04334d9a
AidenGorman <157421522+AidenGorman@users.noreply.github.com>
2024-04-30 15:26:41 -0500

    4/30/24
    .

--- raceGoal.gd
+++ raceGoal.gd
@@ -1,9 +1,14 @@
 extends Node3D
 
 var lap = 0
+@export var next_level = "res://rc_world_2.tscn"
+@onready var HUD = get_tree().get_first_node_in_group("HUD")
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		lap += 1
 	if lap == 3:
 		get_tree().change_scene_to_file("res://rc_world_2.tscn")
+		HUD.popup.visible = true
+		HUD.popup_label.text = "Loading..."
+		await get_tree().create_timer(0.1).timeout 
